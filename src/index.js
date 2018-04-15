3331f5bb42131d84ab33c68d5d665b44bd552a19
Ramon Moraes <ramon.moraes94@gmail.com>
2018-04-14 22:38:27 -0300

    canvas 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import BubbleSort from './bubbleSort';
+import BubbleSort from './bubble-sort';
+import CanvasGraphArray from './canvas-graph-array';
 
-let fakeArray = ["a", 1, "b", true, 34];
+let arr = [1,2,3,4,5];
 
 window.onload = ()=> {
     let bubble = new BubbleSort();
+    let canvas = new CanvasGraphArray({
+        wrapper: document.querySelector('.Canvas-Wrapper'),
+        baseArray: arr,
+    });
+
+    // canvas.drawLine(25, 100);
+    canvas.drawArray(arr);
 }
